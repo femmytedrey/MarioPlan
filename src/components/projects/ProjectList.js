ef880f410f634b8dc8362ef83a1cdace83bbfeb0
femmytedrey <femmytedrey@gmail.com>
2024-01-17 16:25:17 +0100

    dummy text added

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
 
-export const ProjectList = () => {
+const ProjectList = ({ projects }) => {
   return (
     <div className='project-list section'>
-        <ProjectSummary />
-        <ProjectSummary />
-        <ProjectSummary />
-        <ProjectSummary />
+        { projects && projects.map(project => {
+          return(
+            <ProjectSummary project={project} key={project.id} />
+            
+          )
+        })}
     </div>
   )
 }
+
+export default ProjectList
